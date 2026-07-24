eb48ef07fbbb031908a59ea416969d12473d4460
Tempystral <12647331+Tempystral@users.noreply.github.com>
2026-07-24 12:39:51 -0400

    Update donation replicant

--- src/types/schemas/donations.d.ts
+++ src/types/schemas/donations.d.ts
@@ -14,7 +14,7 @@ export interface Donation {
 	campaign_id: string;
 	completed_at: string;
 	created_at?: string;
-	donor_comment: string;
+	donor_comment: string | null;
 	donor_name: string;
 	fundraising_event_id?: string | null;
 	legacy_id?: number;
